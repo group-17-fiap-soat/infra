44dde112cbe2ca1524f45e3005126c4bdb2c157e
Manocchio <guruu.leste@gmail.com>
2025-05-15 01:30:49 -0300

    lambda config

--- lambda.tf
+++ lambda.tf
@@ -1,37 +1,83 @@
-resource "aws_lambda_function" "auth" {
-  function_name = "send-auth-email"
-  # (runtime, role, handler, etc...)
+# Lambda Role
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-postgres-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_apigatewayv2_api" "http_api" {
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  name       = "lambda-logs"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda Function
+resource "aws_lambda_function" "check_user_email" {
+  filename         = "lambda/check_user.zip"
+  function_name    = "check-user-email"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.10"
+  role             = aws_iam_role.lambda_exec.arn
+  timeout          = 10
+  memory_size      = 128
+  source_code_hash = filebase64sha256("lambda/check_user.zip")
+
+  environment {
+    variables = {
+      DB_HOST     = var.db_host
+      DB_NAME     = var.db_name
+      DB_USER     = var.db_user
+      DB_PASSWORD = var.db_password
+      DB_PORT     = var.db_port
+    }
+  }
+
+  # (opcional) caso use um layer com psycopg2
+  # layers = [aws_lambda_layer_version.psycopg2_layer.arn]
+}
+
+# Lambda permission for API Gateway
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.check_user_email.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.auth.execution_arn}/*/*"
+}
+
+# API Gateway HTTP API
+resource "aws_apigatewayv2_api" "auth" {
   name          = "auth-api"
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_integration" "lambda" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.auth.invoke_arn
-  integration_method = "POST"
+# Integração com Lambda
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.auth.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.check_user_email.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
+# Rota: POST /auth
 resource "aws_apigatewayv2_route" "auth_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /auth/send"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+  api_id    = aws_apigatewayv2_api.auth.id
+  route_key = "POST /auth"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
+# Deploy automático
 resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
+  api_id      = aws_apigatewayv2_api.auth.id
   name        = "$default"
   auto_deploy = true
-}
-
-resource "aws_lambda_permission" "allow_apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.auth.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
--- variables.tf
+++ variables.tf
@@ -54,3 +54,10 @@ variable "kubeconfig_path"{
   description = "path de configuração do kubernets"
 }
 
+variable "db_host" {}
+variable "db_name" {}
+variable "db_user" {}
+variable "db_password" {}
+variable "db_port" {
+  default = "5432"
+}

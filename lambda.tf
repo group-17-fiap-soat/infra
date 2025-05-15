71b866a3e4161ccd149a3fa63df67938038bb566
Manocchio <guruu.leste@gmail.com>
2025-05-15 01:25:23 -0300

    terraform lambda config

---
+++ lambda.tf
@@ -0,0 +1,37 @@
+resource "aws_lambda_function" "auth" {
+  function_name = "send-auth-email"
+  # (runtime, role, handler, etc...)
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "auth-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.auth.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "auth_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /auth/send"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.auth.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}

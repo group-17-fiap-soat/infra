bcad6347bda319f1d9735740c2a2a8c31d36e549
Manocchio <guruu.leste@gmail.com>
2025-05-17 03:42:46 -0300

    update lambda function

--- check_user/lambda_function.py
+++ check_user/lambda_function.py
@@ -1,34 +1,46 @@
 import json
 import psycopg2
+import jwt
 import os
+from datetime import datetime, timedelta
 
 def lambda_handler(event, context):
-    try:
-        body = json.loads(event.get("body", "{}"))
-        email = body.get("email")
+    cpf = json.loads(event['body'])['cpf']
 
-        if not email:
-            return {"statusCode": 400, "body": json.dumps({"erro": "E-mail é obrigatório"})}
 
-        conn = psycopg2.connect(
-            host=os.environ['DB_HOST'],
-            dbname=os.environ['DB_NAME'],
-            user=os.environ['DB_USER'],
-            password=os.environ['DB_PASSWORD'],
-            port=os.environ.get('DB_PORT', 5432)
-        )
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASS']
+    )
 
-        cursor = conn.cursor()
-#         cursor.execute("SELECT 1 FROM usuarios WHERE email = %s LIMIT 1", (email,))
-          result = "result"
-#
-        cursor.close()
-        conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT id, name, email FROM tb_customer WHERE cpf = %s", (cpf,))
+    result = cur.fetchone()
 
-        if result:
-            return {"statusCode": 200, "body": json.dumps({"mensagem": "Usuário autenticado com sucesso"})}
-        else:
-            return {"statusCode": 404, "body": json.dumps({"erro": "Usuário não encontrado"})}
+    if not result:
+        return {
+            "statusCode": 401,
+            "body": json.dumps({"message": "CPF não encontrado"})
+        }
 
-    except Exception as e:
-        return {"statusCode": 500, "body": json.dumps({"erro": str(e)})}
+
+    payload = {
+        "sub": str(result[0]),
+        "name": result[1],
+        "email": result[2],
+        "exp": datetime.utcnow() + timedelta(hours=1)
+    }
+
+    token = jwt.encode(payload, os.environ['JWT_SECRET'], algorithm="HS256")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "id": result[0],
+            "name": result[1],
+            "email": result[2],
+            "token": token
+        })
+    }

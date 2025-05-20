f8bfe29629a44122a8d4aafa0d5fb48497499f63
Manocchio <guruu.leste@gmail.com>
2025-05-20 20:56:12 -0300

    add root variables to avoid error before generating it

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {}
+variable "regionDefault" {}
+variable "cluster_name" {}
+variable "cluster_version" {}
+variable "projectName" {}
+variable "nodeGroup" {}
+variable "role" {}
+variable "instanceType" {}
+variable "s3_bucket_name" {}

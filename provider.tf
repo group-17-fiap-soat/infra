1a9732a1773538886ed4deecf06b5080585a621a
Manocchio <guruu.leste@gmail.com>
2025-05-20 19:41:44 -0300

    remove kubernets provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}

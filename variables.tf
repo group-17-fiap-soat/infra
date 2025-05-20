8aa708c5ab8c61df5fe3c12e9714bf89ac00c3ca
Manocchio <guruu.leste@gmail.com>
2025-05-20 19:08:32 -0300

    remove unused variables.tf

--- variables.tf
+++ variables.tf
@@ -13,54 +13,33 @@ variable "cluster_version" {
   default     = "1.32"
 }
 
-variable "db_host" {}
-variable "db_name" {}
-variable "db_user" {}
-variable "db_password" {}
-variable "db_port" {
-  default = "5432"
-}
 
 
-# Nome do projeto / cluster EKS
 variable "projectName" {
   description = "Nome do cluster EKS"
   type        = string
 }
 
-# Nome do Node Group
 variable "nodeGroup" {
   description = "Nome do node group EKS"
   type        = string
 }
 
-# ARN da IAM Role do EKS (geralmente role no AWS Academy)
 variable "role" {
   description = "ARN da role IAM (ex: role)"
   type        = string
 }
 
-# Tipo de instância para o Node Group (ex: t3.small)
 variable "instanceType" {
   description = "Tipo da instância EC2"
   type        = string
 }
 
-# Região base usada para filtrar AZs (ex: us-east-1)
 variable "regionDefault" {
   description = "Região base para ignorar AZ específica (ex: us-east-1)"
   type        = string
 }
 
-variable "accessConfig" {
-  description = "Modo de autenticação do cluster EKS"
-  type        = string
-  default     = "API_AND_CONFIG_MAP"
-}
-
-variable "policyArn" {
-  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-}
 
 variable "principalArn" {
   default = "arn:aws:iam::361598269712:role/RootRole"

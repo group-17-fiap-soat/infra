3e0b22c245f9bb852d36f80c1322b5af77d1cc76
Manocchio <guruu.leste@gmail.com>
2025-05-20 19:46:00 -0300

    prevent destroy s3 bucket

--- s3.tf
+++ s3.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
   force_destroy = true
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "dev"

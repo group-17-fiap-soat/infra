ec64fc1785451ebb2dd881fefb8544788920cece
Manocchio <guruu.leste@gmail.com>
2025-05-20 19:01:54 -0300

    add deploy workflow to destroy and to apply infrastructure

---
+++ s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "dev"
+  }
+}
+
+variable "s3_bucket_name" {
+  default = "terraform-pipeline-bucket"
+}

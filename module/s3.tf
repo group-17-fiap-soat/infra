082f32d73ec2e51ba65abbe9f04688481b89de3c
Manocchio <guruu.leste@gmail.com>
2025-05-24 03:44:31 -0300

    remove prevent destroy to avoid billing

--- module/s3.tf
+++ module/s3.tf
@@ -2,10 +2,6 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
   force_destroy = true
 
-  lifecycle {
-    prevent_destroy = true
-  }
-
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "dev"

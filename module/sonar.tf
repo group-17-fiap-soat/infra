e4998913dc82417525cee7bce37263368aadcd04
Manocchio <guruu.leste@gmail.com>
2025-07-25 12:19:02 -0300

    expose port 9000 with sonarqube

--- module/sonar.tf
+++ module/sonar.tf
@@ -6,6 +6,39 @@ resource "aws_instance" "sonar" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   subnet_id              = aws_subnet.public[0].id
 
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io docker-compose -y
+              systemctl start docker
+              systemctl enable docker
+
+              # Criar docker-compose
+              mkdir -p /opt/sonarqube
+              cat <<EOL > /opt/sonarqube/docker-compose.yml
+              version: "3"
+              services:
+                sonarqube:
+                  image: sonarqube:community
+                  container_name: sonarqube
+                  ports:
+                    - "9000:9000"
+                  environment:
+                    - SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true
+                  volumes:
+                    - sonarqube_data:/opt/sonarqube/data
+                    - sonarqube_extensions:/opt/sonarqube/extensions
+                  restart: unless-stopped
+
+              volumes:
+                sonarqube_data:
+                sonarqube_extensions:
+              EOL
+
+              cd /opt/sonarqube
+              docker-compose up -d
+              EOF
+
   tags = {
     Name = "fastfood-sonar"
   }
